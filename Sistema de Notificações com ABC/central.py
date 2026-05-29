ceacc49812b1f00d8fcd4e82822ac7e848a69607
Marcos Oliveira <marcosilva.mo91@gmail.com>
2026-05-29 00:26:32 -0300

    Create central.py

---
+++ Sistema de Notificações com ABC/central.py
@@ -0,0 +1,10 @@
+class CentralNotificacoes:
+    def __init__(self):
+        self.notificadores = []
+ 
+    def adicionar_notificador(self, notificador):
+        self.notificadores.append(notificador)
+ 
+    def enviar_para_todos(self, mensagem):
+        for notificador in self.notificadores:
+            notificador.notificar(mensagem)
